55f79fccb62ed43406dd660fa0c71d4f5d70d308
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-09 23:57:54 +0530

    [Divya]: Snake Ladder Game play

--- Play.cs
+++ Play.cs
@@ -12,6 +12,9 @@ namespace SnakeLadderPro_Day2
         {
             Console.WriteLine("Start the Snake and Ladder Game");
             Console.WriteLine("Single Player at Start Position 0");
+            Random random = new Random();
+            int RollDice = random.Next(1, 6);
+            Console.WriteLine("Player 1 rolls the die and get:" + RollDice);
         }
     }
 }

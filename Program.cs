ab365527b8dae9286185b02ea553636bbeed0c76
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-09 23:40:29 +0530

    [Divya]: Snake Ladder Program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using SnakeLadderPro_Day2;
+
+namespace SnakeLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake Ladder Program");
+
+            Play game = new Play();
+            game.Start();
+
+        }
+    }
+}
